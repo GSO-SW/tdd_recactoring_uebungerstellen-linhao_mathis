9531cf6c9f6414200f784d357fe2d35c448afc3d
Linhao Feng <fenglinhao1238@gmail.com>
2022-12-22 08:44:48 +0100

    Projekt hinzufügen

---
+++ 22-12-2022 SWE Refactoring/22-12-2022 SWE Refactoring/Rechteck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_12_2022_SWE_Refactoring
+{
+    internal class Rechteck
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Rechteck(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double A
+        {
+            get{ return a; }
+        }
+        public double B
+        {
+            get{ return b; }
+        }
+
+        public double C 
+        { 
+            get{ return a * b ; }
+        }
+    }
+}
